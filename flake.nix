13b3596c895dd4ea9be4ef005d07a40375eb3883
Exscallion <github-commit.trickle531@passmail.net>
2025-11-29 20:17:42 +0100

    Added flake.nix

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "My NixOS install";
+  inputs = {
+    nixpkgs = "github:nixos/nixpkgs/nixos-25.11";
+    home-manager = {
+      url = "github.com:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follow = "nixpkgs";
+    };
+  };
+  
+  outputs = { self, nixpkgs, ... } @inputs:
+  let 
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system}
+  in {
+    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+      inherit system;
+      specialArgs = { inherit inputs; };
+      modules = [
+        (
+          {
+            config,
+            pkgs,
+            ...
+          }:
+          {
+            nixpkgs.config.allowUnfree = true;
+          }
+        )
+        ./hosts/default/configuration.nix
+        inputs.home-manager.nixosModules.default
+      ];
+    };
+  };
+}
